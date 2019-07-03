d56f923acce2fc64e42a95b0dc6c31b0362bbae0
Ben <allenben@protonmail.com>
2019-07-03 14:36:28 -0500

    Created HeaderTitle

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+const BoldText = props => {
+    <h6>Lambda School</h6>
+}
+
+const LightText = props => {
+    <p>@LambdaSchool * 26 Jan</p>
+}
+
+const Title = props => {
+    <div>
+        <BoldText />
+        <LightText />
+    </div>
+}
